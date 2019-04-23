c207181fe216ddf6dcc7a7fcaf0fd38f3c0fed20
Mathis Gendron <mathis.gendron@etu.u-bordeaux.fr>
2019-04-23 11:56:11 +0200

    Premier push des fichiers sources Problème courant : Une bonne partie des fonctionnalités minimales sont implémentées côté serveur mais des bugs empêchant le client d'aller plus loin que le renseignement d'un nickname sont encore présents. Différents clients sont encore asynchrones.

---
+++ client.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+
+"""
+Ecrit par le prof au tableau (algo)
+
+CLIENT.PY
+=========
+socket(v4, TCP)
+connect
+select([s, sys.stdin])
+  if s in read...
+    d = s.recv(1024)
+    print(d)
+  if s in exceptional...
+    s.close()
+  if s = sys.stdin
+    l = s.recv(...)
+    l.split()
+    .....COMMAND PARSE 2
+
+SERVEUR.PY
+==========
+socket(v4, TCP)
+listen(so)
+[serv]
+select
+  if s in read...
+    if s = serv
+    c = s.accept()
+  else
+    s.recv()
+    parse command
+    envoyer MSG à tous les autres ml du channel
+"""
+###########
+# MODULES #
+###########
+
+import socket
+import select
+import threading
+import sys
+
+####################
+# GLOBAL VARIABLES #
+####################
+
+LOCALHOST=("127.0.0.1",1459)
+USAGE="""USAGE :
+  -h | --help
+    display this help
+  -v | --version
+    display the version
+  -a | --addr
+    change the address (by default localhost)
+"""
+VERSION="0.01"
+LIMIT=2048
+address = LOCALHOST
+
+def ERROR(s):
+    print("Error : ", s)
+    exit()
+
+###################
+# PARSE ARGUMENTS #
+###################
+    
+argv = sys.argv
+argc = len(sys.argv)
+for i in range(1,argc):
+    if argv[i] in ("-a", "--addr"):
+        i+=1
+        if i >= argc:
+            ERROR(argv[i-1] + " needs an argument")
+        address = (argv[i], address[1])
+    elif argv[i] in ("-h", "--help"):
+        print(USAGE)
+        exit()
+    elif argv[i] in ("-v", "--version"):
+        print("Version " + VERSION)
+        exit()
+
+
+########
+# CODE #
+########
+
+# Create socket
+s = socket.socket()
+s.connect(address)
+
+liste = [s, sys.stdin]
+
+# Main loop
+while True:
+  reading, writing, exceptional = select.select(liste,[],[])
+  for r in liste:
+    ##problem: stdin ends up in reading and can't do a recv on it
+      if r in reading:
+        if r!=sys.stdin:
+          d = r.recv(LIMIT).decode("utf-8")
+          print(d)
+      if r in exceptional:
+        r.close()
+        exit()
+      if r == sys.stdin:
+        l = r.readline()
+        if l[0] == '/': #command
+            l = l[1:] + " "
+            print(l)
+        else:
+            l = "PRINT " + l
+        s.send(l.encode("utf-8"))
+
+
+"""
+            l.split(" ", 1)
+            l[0] == "MSG"
+            l[1] == "<nick> <message>"
+            l[1].split(" ", 1)
+"""
---
+++ server.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python3
+
+###########
+# MODULES #
+###########
+
+import socket
+import select
+import threading
+import sys
+
+####################
+# GLOBAL VARIABLES #
+####################
+
+LOCALHOST=("127.0.0.1",1459)
+USAGE="""USAGE :
+  -h | --help
+    display this help
+  -v | --version
+    display the version
+  -a | --addr
+    change the address (by default localhost)
+"""
+
+VERSION="0.02"
+LIMIT=2048
+
+def ERROR(s):
+    print("Error : ", s)
+    exit()
+
+###################
+# PARSE ARGUMENTS #
+###################
+    
+argv = sys.argv
+argc = len(sys.argv)
+for i in range(1,argc):
+    if argv[i] in ("-a", "--addr"):
+        i+=1
+        if i >= argc:
+            ERROR(argv[i-1] + " needs an argument")
+        address = (argv[i], address[1])
+    elif argv[i] in ("-h", "--help"):
+        print(USAGE)
+        exit()
+    elif argv[i] in ("-v", "--version"):
+        print("Version " + VERSION)
+        exit()
+
+
+########
+# CODE #
+########
+
+##Function used to send a message to all clients, sock1 is used as an exception to avoid broken pipe error, sock2 is used
+##to make an exception for another socket if needed.
+def send_all(l,sock1,sock2,msg):
+    for i in l:
+        if i!=sock1 and i!=sock2:
+            i.send(msg.encode("utf-8"))
+
+##Function used to send a message to the clients in a given chanel
+def send_cnl(chan,msg):
+    for i in chan:
+        i.send(msg.encode("utf-8"))
+        
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind(('',1459))
+s.listen(1)
+##List of server and clients sockets
+lsock=[s]
+##Dictionnary of chat channels
+lchan={}
+##Dictionnary of all clients
+lclt={}
+while 1<2:
+    reading,writing,exceptional=select.select(lsock,[],lsock)
+    for i in reading:
+        if i==s:
+            news,addrnews=i.accept()
+            #Prompt for a nickname, asks for a proper nickname as long as it's not given.
+            temp=addrnews
+            while addrnews==temp:
+                news.send("Specify nickname with /NICK command.\n".encode("utf-8"))
+                nick=news.recv(LIMIT).decode("utf-8")
+                if nick[:5]==("NICK "):
+                    given_nick=nick.partition(' ')[2].rstrip(' \n')
+                    for k in lclt:
+                        if lclt[k]==given_nick:
+                            news.send("Nickname already taken.\n")
+                            break    
+                    addrnews=given_nick
+            lsock+=[news,]
+            lclt[news]=addrnews
+            news.send("List of channels:\n".encode("utf-8"))
+            if len(lchan)!=0:
+                for k in lchan:
+                    news.send("{0}\n".format(k).encode("utf-8"))
+            else:
+                news.send("*No channels*\n".encode("utf-8"))
+            news.send("Use /LIST command to display this list again.\nJoin a channel with /JOIN <channel_name> command before continuing.\n".encode("utf-8"))
+            ##Prompts for a channel to join, asks again until a valid channel name is given.
+            join_cnl=""
+            while len(join_cnl)==0:
+                join_cmd=news.recv(LIMIT).decode("utf-8")
+                if join_cmd[:5]=="JOIN ":
+                    join_cnl=join_cmd.partition(' ')[2].rstrip(' \n')
+                    if len(join_cnl)!=0 and join_cnl not in lchan:
+                        lchan[join_cnl]={}
+                    lchan[join_cnl][news]=lclt[news]
+                    send_cnl(lchan[join_cnl],"JOIN {0} {1}\n".format(join_cnl,lclt[news]))
+        else:
+            decmsg=i.recv(2042).decode("utf-8")
+            ##Makes the client leave if he presses enter with no message
+            if len(decmsg)<=1:
+                    leave_addr=""
+                    leave_msg="PART {0}\n".format(lclt[i])
+                    i.close()
+                    lsock.remove(i)
+                    lclt.pop(i)
+                    for k in lchan:
+                        lchan[k].pop(i)
+                    send_all(lsock,s,i,leave_msg)
+                    break
+            command, argument = decmsg.split(" ", 1)
+            argument = argument.rstrip(' \n')
+            print(command)
+            print(argument)
+            ##Code for MSG feature: sending messages
+            if command == "PRINT":
+                if argument!="":
+                    for k in lchan:
+                        if i in lchan[k]:
+                            send_cnl(lchan[k],argument)
+            elif command == "MSG":
+                chan_msg=decmsg.partition(' ')[2]
+                if chan_msg.startswith("all "):
+                    send_all(lsock,s,i,decmsg.lstrip('/'))
+                else:
+                    ##Extracting the channel name
+                    chan=chan_msg.partition(' ')[0].rstrip(' \n')
+                    if chan in lchan and i in lchan[chan]:
+                        send_cnl(lchan[chan],decmsg.lstrip('/'))
+            ##Code for LIST feature: listing channels
+            if command == "WHO":
+                for k in lchan:
+                    if i in lchan[k]:
+                        for r in lchan[k]:
+                            i.send("{0}\n".format(lchan[k][r]).encode("utf-8"))
+            if command == "LIST":
+                i.send("List of channels:\n".encode("utf-8"))
+                for k in lchan:
+                    i.send("{0}\n".format(k).encode("utf-8"))
+            ##Code for KILL feature: removing a client from the chat
+            if command == "KILL":
+                kck_addr=argument
+                temp=0
+                for k in lclt:
+                    if lclt[k]==kck_addr:
+                        k.send("Seeya\n".encode("utf-8"))
+                        k.close()
+                        lsock.remove(k)
+                        temp=k
+                lclt.pop(temp)
+            ##Code for JOIN feature: joining a channel
+            if command == "JOIN":
+                join_cnl=decmsg.partition(' ')[2].rstrip(' \n')
+                if join_cnl not in lchan:
+                    lchan[join_cnl]={}
+                lchan[join_cnl][i]=lclt[i]
+                send_cnl(lchan[join_cnl],"JOIN {0} {1}\n".format(join_cnl,lclt[i]))
+            ##Code for PART feature: leaving a channel
+            if command == "PART":
+                part_cnl=argument
+                if part_cnl in lchan and i in lchan[part_cnl]:
+                    send_cnl(lchan[part_cnl],"PART {0} {1}\n".format(part_cnl,lclt[i]))
+                    lchan[part_cnl].pop(i)
+            ##Code for KICK function: removing a client from a channel
+            if command == "KICK":
+                kick_arg=argument
+                kick_cnl=kick_arg.partition(' ')[0].rstrip(' \n')
+                kick_nck=kick_arg.partition(' ')[2].rstrip(' \n')
+                if kick_cnl!="default" and kick_cnl in lchan and i in lchan[kick_cnl]:
+                    temp=0
+                    for k in lchan[kick_cnl]:
+                        if lchan[kick_cnl][k]==kick_nck:
+                            temp=k
+                    lchan[kick_cnl].pop(temp)
